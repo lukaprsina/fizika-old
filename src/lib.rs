a045610fe9d4dd1e14b8850191afed68d32ead28
Luka Pršina <luk.prsina@gmail.com>
2022-04-12 17:06:21 +0200

    naivni poskus dela

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod ast;
-pub mod parser;
 
 pub use crate::ast::{Node, Operator};
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use math_eval::{parser::ExpressionParser, Node};
-use pest::RuleType;
-
-fn parse_csv(input_str: &str) -> math_eval::parser::Result<Node> {
-    // Parse the input into `Nodes`
-    let inputs = ExpressionParser::parse(Rule::file, input_str)?;
-    // There should be a single root node in the parsed tree
-    let input = inputs.single()?;
-    // Consume the `Node` recursively into the final value
-    ExpressionParser::file(input)
-}
+use math_eval::{Node, Operator};
 
 fn main() {
-    let parsed = parse_csv("1+1").unwrap();
+    // (6 + a)^2
+    let a = Node::Binary {
+        op: Operator::Power,
+        lhs: Box::new(Node::Binary {
+            op: Operator::Plus,
+            lhs: Box::new(Node::Number(6.)),
+            rhs: Box::new(Node::Number(1.)),
+            // rhs: Box::new(Node::Variable("a".to_string())),
+        }),
+        rhs: Box::new(Node::Number(2.)),
+    };
+
+    // f(x) = log(2, x) + 3x - 2
+    let _ = Node::Binary {
+        op: Operator::Equals,
+        lhs: Box::new(Node::Function {
+            name: "f".to_string(),
+            args: vec![Node::Variable("x".to_string())],
+        }),
+        rhs: Box::new(Node::Binary {
+            op: Operator::Minus,
+            lhs: Box::new(Node::Binary {
+                op: Operator::Plus,
+                lhs: Box::new(Node::Function {
+                    name: "log".to_string(),
+                    args: vec![Node::Number(2.), Node::Variable("x".to_string())],
+                }),
+                rhs: Box::new(Node::Binary {
+                    op: Operator::Times,
+                    lhs: Box::new(Node::Number(3.)),
+                    rhs: Box::new(Node::Variable("x".to_string())),
+                }),
+            }),
+            rhs: Box::new(Node::Number(2.)),
+        }),
+    };
+
+    println!("{}", a.eval());
 }
