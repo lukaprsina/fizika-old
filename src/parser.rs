8ebec1d943b71fbe8d779d89342dd909f34165b0
Luka Pršina <prsinaluka@gmail.com>
2022-04-17 11:57:46 +0200

    before using duplicate

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,12 @@
+use num::{BigInt, BigRational, Complex, Rational64 };
 use nom::{
-    branch::alt, bytes::complete::{tag, tag_no_case}, character::complete::multispace0, error::ParseError,
-    sequence::delimited, IResult,
+    branch::alt, bytes::complete::{tag, tag_no_case}, character::complete::{multispace0, one_of, char}, error::ParseError,
+    sequence::{delimited, preceded, terminated}, IResult, combinator::{map_res, recognize}, multi::{many1, many0},
 };
 
+// use duplicate
+
+#[derive(Debug)]
 pub enum Operation {
     Add,
     Subtract,
@@ -19,6 +23,7 @@ pub enum Operation {
     GreaterThan,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Unit {
     Degree,
     Second,
@@ -28,6 +33,15 @@ pub enum Unit {
     Custom(String),
 }
 
+#[derive(Debug)]
+pub enum Number {
+    BigInt,
+    BigRational,
+    Complex,
+    Rational64
+}
+
+#[derive(Debug)]
 pub enum Token {
     Binary(Operation),
     Unary(Operation),
@@ -66,28 +80,50 @@ fn parse_operation(input: &str) -> IResult<&str, &str> {
     ))(input)
 }
 
-fn parse_unit(input: &str) -> IResult<&str, &str> {
+fn parse_unit(input: &str) -> IResult<&str, Unit> {
     alt((
-        ws(tag_no_case("°")),
-        ws(tag_no_case("s")),
-        ws(tag_no_case("m")),
-        ws(tag_no_case("rad")),
-        ws(tag_no_case("grad")),
-        ws(tag_no_case("custom")),
+        map_res(ws(tag_no_case("°")), |_| Ok::<Unit, ()>(Unit::Degree)),
+        map_res(ws(tag_no_case("\'")), |_| Ok::<Unit, ()>(Unit::Second)),
+        map_res(ws(tag_no_case("\'\'")), |_| Ok::<Unit, ()>(Unit::Minute)),
+        map_res(ws(tag_no_case("rad")), |_| Ok::<Unit, ()>(Unit::Radian)),
+        map_res(ws(tag_no_case("grad")), |_| Ok::<Unit, ()>(Unit::Gradian)),
+        map_res(ws(tag_no_case("custom")), |s: &str| Ok::<Unit, ()>(Unit::Custom(s.to_string()))),
     ))(input)
+}
+
+
+// TODO: function to convert to Token::Number
+fn parse_hexadecimal(input: &str) -> IResult<&str, i64> {
+    map_res(
+      preceded(
+        alt((tag("0x"), tag("0X"))),
+        recognize(
+          many1(
+            terminated(one_of("0123456789abcdefABCDEF"), many0(char('_')))
+          )
+        )
+      ),
+      |out: &str| i64::from_str_radix(&str::replace(&out, "_", ""), 16)
+    )(input)
+  }
+
+fn parse_number(input: &str) -> IResult<&str, &str> {
+    tag("a")(input)
+}
+
+fn parse_variable(input: &str) -> IResult<&str, &str> {
+    tag("a")(input)
+}
+
+fn parse_function(input: &str) -> IResult<&str, &str> {
+    tag("a")(input)
 }
 
 fn parse_token(input: &str) -> IResult<&str, &str> {
     alt((
-        ws(parse_operation),
-        ws(parse_unit),
         ws(tag("(")),
         ws(tag(")")),
         ws(tag(",")),
-        // TODO
-        ws(tag("number")),
-        ws(tag("variable")),
-        ws(tag("function")),
     ))(input)
 }
 
@@ -98,5 +134,10 @@ mod test {
     #[test]
     fn test_operation() {
         assert_eq!(parse_operation("    \n\t+  a  "), Ok(("a  ", "+")));
+    }
+
+    #[test]
+    fn test_unit() {
+        assert_eq!(parse_unit("    \n\t\'    "), Ok(("", Unit::Second)));
     }
 }
