ca80d31a3a40bb1dc2e08452f22e8b58a01a0169
Luka Pršina <prsinaluka@gmail.com>
2022-04-18 19:04:26 +0200

    before number enum

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,7 @@ use nom::{
     sequence::{delimited, pair, preceded, terminated, tuple},
     IResult,
 };
-// use num::{BigInt, BigRational, Complex, Rational64};
+use num::{BigInt, BigRational, Complex, Rational64};
 
 #[derive(Debug, PartialEq)]
 pub enum Operation {
@@ -52,19 +52,19 @@ pub enum Token {
     LeftParenthesis,
     RightParenthesis,
     Comma,
-    Number(f64, Option<Unit>),
+    Number(Number, Option<Unit>),
     Variable(String),
     Function(String, Option<usize>),
 }
 
-pub fn peol_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
+fn parse_eol_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
     value(
         (), // Output is thrown away.
         pair(tag(r"\/\/"), is_not("\n\r")),
     )(i)
 }
 
-pub fn pinline_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
+fn parse_inline_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
     value(
         (), // Output is thrown away.
         tuple((tag("/*"), take_until("*/"), tag("*)"))),
@@ -80,76 +80,45 @@ where
     delimited(multispace0, inner, multispace0)
 }
 
-/* fn parse_test<ParserInput, P, F, FuncInput, FuncOutput, FuncErr, RetInput, RetOutput, RetError>(
-    parser: P,
-    function: F
-) -> impl FnMut(RetInput) -> IResult<RetInput, RetOutput, RetError>
-where
-    RetInput: Clone,
-    P: Parser<ParserInput, FuncInput, RetError>,
-    F: FnMut(FuncInput) -> Result<FuncOutput, FuncErr>
-{
-    map_res(ws(parser), function)
-}
-
-pub fn parse_test<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
-    value(
-        (), // Output is thrown away.
-        tuple((tag("/*"), take_until("*/
-"), tag("*)"))),
-    )(i)
-}
-
-fn test() {
-    let _a = alt((parse_test(), parse_test()))("a");
-} */
-
 fn parse_and_map<'a, OutputType, FuncError>(
     name: &'a str,
     function: impl FnMut(&'a str) -> Result<OutputType, FuncError>,
 ) -> impl FnMut(&'a str) -> IResult<&'a str, OutputType> {
-    map_res(ws(tag(name)), function)
+    map_res(ws(tag_no_case(name)), function)
 }
 
 fn parse_operation(input: &str) -> IResult<&str, Operation> {
     alt((
         parse_and_map("+", |_| Ok::<Operation, ()>(Operation::Add)),
         // map_res(ws(tag("+")), |_| Ok::<Operation, ()>(Operation::Add)),
-        map_res(ws(tag("-")), |_| Ok::<Operation, ()>(Operation::Subtract)),
-        map_res(ws(tag("*")), |_| Ok::<Operation, ()>(Operation::Multiply)),
-        map_res(ws(tag("/")), |_| Ok::<Operation, ()>(Operation::Divide)),
-        map_res(ws(tag("%")), |_| Ok::<Operation, ()>(Operation::Mod)),
-        map_res(ws(tag("^")), |_| Ok::<Operation, ()>(Operation::Power)),
-        map_res(ws(tag("!")), |_| Ok::<Operation, ()>(Operation::Factorial)),
-        map_res(ws(tag("=")), |_| Ok::<Operation, ()>(Operation::Equal)),
-        map_res(ws(tag("!=")), |_| Ok::<Operation, ()>(Operation::NotEqual)),
-        map_res(ws(tag("<")), |_| Ok::<Operation, ()>(Operation::LessThan)),
-        map_res(ws(tag("<=")), |_| {
-            Ok::<Operation, ()>(Operation::LessThanOrEqual)
-        }),
-        map_res(ws(tag(">=")), |_| {
-            Ok::<Operation, ()>(Operation::GreaterThanOrEqual)
-        }),
-        map_res(ws(tag(">")), |_| {
-            Ok::<Operation, ()>(Operation::GreaterThan)
-        }),
+        parse_and_map("-", |_| Ok::<Operation, ()>(Operation::Subtract)),
+        parse_and_map("*", |_| Ok::<Operation, ()>(Operation::Multiply)),
+        parse_and_map("/", |_| Ok::<Operation, ()>(Operation::Divide)),
+        parse_and_map("%", |_| Ok::<Operation, ()>(Operation::Mod)),
+        parse_and_map("^", |_| Ok::<Operation, ()>(Operation::Power)),
+        parse_and_map("!", |_| Ok::<Operation, ()>(Operation::Factorial)),
+        parse_and_map("=", |_| Ok::<Operation, ()>(Operation::Equal)),
+        parse_and_map("!=", |_| Ok::<Operation, ()>(Operation::NotEqual)),
+        parse_and_map("<", |_| Ok::<Operation, ()>(Operation::LessThan)),
+        parse_and_map("<=", |_| Ok::<Operation, ()>(Operation::LessThanOrEqual)),
+        parse_and_map(">=", |_| Ok::<Operation, ()>(Operation::GreaterThanOrEqual)),
+        parse_and_map(">", |_| Ok::<Operation, ()>(Operation::GreaterThan)),
     ))(input)
 }
 
 fn parse_unit(input: &str) -> IResult<&str, Unit> {
     alt((
-        map_res(ws(tag_no_case("°")), |_| Ok::<Unit, ()>(Unit::Second)),
-        map_res(ws(tag_no_case("\'")), |_| Ok::<Unit, ()>(Unit::Second)),
-        map_res(ws(tag_no_case("\'\'")), |_| Ok::<Unit, ()>(Unit::Minute)),
-        map_res(ws(tag_no_case("rad")), |_| Ok::<Unit, ()>(Unit::Radian)),
-        map_res(ws(tag_no_case("grad")), |_| Ok::<Unit, ()>(Unit::Gradian)),
-        map_res(ws(tag_no_case("custom")), |s: &str| {
+        parse_and_map("°", |_| Ok::<Unit, ()>(Unit::Second)),
+        parse_and_map("\'", |_| Ok::<Unit, ()>(Unit::Second)),
+        parse_and_map("\'\'", |_| Ok::<Unit, ()>(Unit::Minute)),
+        parse_and_map("rad", |_| Ok::<Unit, ()>(Unit::Radian)),
+        parse_and_map("grad", |_| Ok::<Unit, ()>(Unit::Gradian)),
+        parse_and_map("custom", |s: &str| {
             Ok::<Unit, ()>(Unit::Custom(s.to_string()))
         }),
     ))(input)
 }
 
-// TODO: function to convert to Token::Number
 fn parse_hexadecimal(input: &str) -> IResult<&str, i64> {
     map_res(
         preceded(
@@ -163,46 +132,64 @@ fn parse_hexadecimal(input: &str) -> IResult<&str, i64> {
     )(input)
 }
 
-fn octal(input: &str) -> IResult<&str, &str> {
-    preceded(
-        alt((tag("0o"), tag("0O"))),
-        recognize(many1(terminated(one_of("01234567"), many0(char('_'))))),
+fn parse_octal(input: &str) -> IResult<&str, i64> {
+    map_res(
+        preceded(
+            alt((tag("0o"), tag("0O"))),
+            recognize(many1(terminated(one_of("01234567"), many0(char('_'))))),
+        ),
+        |out: &str| i64::from_str_radix(&str::replace(&out, "_", ""), 8),
     )(input)
 }
 
-fn binary(input: &str) -> IResult<&str, &str> {
-    preceded(
-        alt((tag("0b"), tag("0B"))),
-        recognize(many1(terminated(one_of("01"), many0(char('_'))))),
+fn parse_binary(input: &str) -> IResult<&str, i64> {
+    map_res(
+        preceded(
+            alt((tag("0b"), tag("0B"))),
+            recognize(many1(terminated(one_of("01"), many0(char('_'))))),
+        ),
+        |out: &str| i64::from_str_radix(&str::replace(&out, "_", ""), 2),
     )(input)
 }
 
-fn decimal(input: &str) -> IResult<&str, &str> {
-    recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
+fn parse_decimal(input: &str) -> IResult<&str, i64> {
+    map_res(
+        recognize(many1(terminated(one_of("0123456789"), many0(char('_'))))),
+        |out: &str| i64::from_str_radix(&str::replace(&out, "_", ""), 10),
+    )(input)
 }
 
-fn float(input: &str) -> IResult<&str, &str> {
+fn parse_float(input: &str) -> IResult<&str, f64> {
     alt((
         // Case one: .42
-        recognize(tuple((
-            char('.'),
-            decimal,
-            opt(tuple((one_of("eE"), opt(one_of("+-")), decimal))),
-        ))), // Case two: 42e42 and 42.42e42
-        recognize(tuple((
-            decimal,
-            opt(preceded(char('.'), decimal)),
-            one_of("eE"),
-            opt(one_of("+-")),
-            decimal,
-        ))), // Case three: 42. and 42.42
-        recognize(tuple((decimal, char('.'), opt(decimal)))),
+        map_res(
+            recognize(tuple((
+                char('.'),
+                parse_decimal,
+                opt(tuple((one_of("eE"), opt(one_of("+-")), parse_decimal))),
+            ))),
+            |out: &str| out.parse::<f64>(),
+        ), // Case two: 42e42 and 42.42e42
+        map_res(
+            recognize(tuple((
+                parse_decimal,
+                opt(preceded(char('.'), parse_decimal)),
+                one_of("eE"),
+                opt(one_of("+-")),
+                parse_decimal,
+            ))),
+            |out: &str| out.parse::<f64>(),
+        ), // Case three: 42. and 42.42
+        map_res(
+            recognize(tuple((parse_decimal, char('.'), opt(parse_decimal)))),
+            |out: &str| out.parse::<f64>(),
+        ),
     ))(input)
 }
 
-fn parse_number(input: &str) -> IResult<&str, &str> {
-    tag("a")(input)
-}
+/* fn parse_number(input: &str) -> IResult<&str, i64> {
+    alt((parse_binary(input),))(input)
+} */
 
 fn parse_variable(input: &str) -> IResult<&str, &str> {
     tag("a")(input)
