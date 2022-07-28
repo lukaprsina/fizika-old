a4912dd5edd10efbed179e8fe05c105c1ec4c7a0
Luka Pršina <prsinaluka@gmail.com>
2022-07-28 13:08:50 +0200

    is_same and simplify

--- src/actions/simplify.rs
+++ src/actions/simplify.rs
@@ -1,3 +1,16 @@
+use crate::ast::Equation;
+
 pub trait Simplify {
     fn simplify(&mut self);
+}
+
+impl Simplify for Equation {
+    fn simplify(&mut self) {
+        let mut borrowed_app = self.app.borrow_mut();
+        let context = borrowed_app.get_context_mut(self.context).unwrap();
+
+        for uuid in self.uuids.iter() {
+            let expression = context.get_expression_mut(*uuid).unwrap();
+        }
+    }
 }
