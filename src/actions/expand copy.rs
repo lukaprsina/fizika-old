c42dafc7cf7404bc086b481611c5253ab8a1a3bf
Luka Pršina <prsinaluka@gmail.com>
2022-08-21 22:25:11 +0200

    expand new revision

--- src/actions/expand copy.rs
+++ src/actions/expand copy.rs
@@ -5,43 +5,71 @@ use tracing::info;
 use crate::ast::{product::Product, Element, Expression, NodeOrExpression, Sign};
 
 impl Expression {
+    // TODO: own self
     pub fn expand(&mut self) {
-        let mut has_expression = false;
         for product in self.products.iter_mut() {
-            for side in [&mut product.numerator, &mut product.denominator] {
-                for element in side {
-                    if let NodeOrExpression::Expression(expr) = &mut element.node_or_expression {
-                        has_expression = true;
-                        expr.expand();
+            // 7 * a * (2 - a) / 2 * (b + 4) * 4
+            let mut has_expression = false;
+            let mut last_element: Option<Element> = None;
+
+            for (side_pos, side) in [&mut product.numerator, &mut product.denominator]
+                .into_iter()
+                .enumerate()
+            {
+                let mut new_side: Vec<Element> = vec![];
+                let mut nodes: Vec<Element> = vec![];
+
+                for mut element in side.clone() {
+                    let is_node = match &mut element.node_or_expression {
+                        NodeOrExpression::Node(_) => {
+                            nodes.push(element.clone());
+                            true
+                        }
+                        NodeOrExpression::Expression(expression) => {
+                            has_expression = true;
+                            expression.expand();
+                            info!("Expanded: {}", element);
+                            false
+                        }
+                    };
+
+                    let new_element = match last_element {
+                        Some(last) => match side_pos {
+                            0 => last * element,
+                            1 => last / element,
+                            _ => unreachable!(),
+                        },
+                        None => element,
+                    };
+
+                    last_element = Some(new_element.clone());
+
+                    info!("Is node?: {}", is_node);
+                    info!("Has expression?: {}", has_expression);
+                    info!("Multiplied: {}\n", new_element);
+
+                    if !is_node || new_element.to_string() == "(3 * 2)/5 - (4 * 2)/5" {
+                        new_side.push(new_element);
                     }
                 }
-            }
-        }
 
-        info!("Element {}: {}", self, has_expression);
-
-        if has_expression {
-            for product in self.products.iter_mut() {
-                let mut new_expression = Expression::new(vec![]);
-                let mut last_element: Option<Element> = None;
-
-                for (side_pos, side) in [&mut product.numerator, &mut product.denominator]
-                    .into_iter()
-                    .enumerate()
-                {
-                    for element in side {
-                        let new_element = match &mut last_element {
-                            Some(last) => match side_pos {
-                                0 => last.clone() * element.clone(),
-                                1 => last.clone() / element.clone(),
-                                _ => unreachable!(),
-                            },
-                            None => element.clone(),
-                        };
-
-                        last_element = Some(new_element);
-                    }
+                info!("Has expression?: {}", has_expression);
+                for node in nodes.iter() {
+                    info!("Node: {}", node);
                 }
+
+                side.clear();
+
+                if !has_expression {
+                    side.append(&mut nodes);
+                }
+
+                side.append(&mut new_side);
+                for element in side.iter() {
+                    info!("Element: {}", element);
+                }
+
+                println!("\n\n");
             }
         }
     }
