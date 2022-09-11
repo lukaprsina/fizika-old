baa0412fdc821fa99a13a9e960359b3fd032dab6
Luka Pršina <luk.prsina@gmail.com>
2022-09-11 22:54:54 +0200

    mathlive and wysiwyg

--- web/pages/wysiwyg.tsx
+++ web/pages/wysiwyg.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { EditorState, Modifier } from 'draft-js';
 import { NextPage } from 'next';
 import dynamic from 'next/dynamic';
+// import { MathfieldElement } from 'mathlive';
 
 const NoSSREditor = dynamic(() =>
     import('react-draft-wysiwyg').then((mod) => mod.Editor), {
@@ -9,12 +10,39 @@ const NoSSREditor = dynamic(() =>
 })
 
 const WYSIWYG: NextPage = () => {
-    return <NoSSREditor
-        wrapperClassName="demo-wrapper"
-        editorClassName="demo-editor"
-        // eslint-disable-next-line react/jsx-key
-        toolbarCustomButtons={[<CustomOption />]}
-    />
+    const mathRef = useRef<any>();
+    useEffect(() => {
+        const a = async () => {
+            const mathlive = (await import('mathlive'))
+
+            let mfe = new mathlive.MathfieldElement();
+            mfe.setOptions({
+                virtualKeyboards: "all",
+                virtualKeyboardMode: "manual",
+                fontsDirectory: "/assets/fonts",
+                soundsDirectory: "/assets/sounds",
+            })
+            mfe.value = "x=\\frac{-b\\pm\\sqrt{b ^ 2 - 4ac}}{2a}";
+            // mfe.executeCommand(['switchMode', 'math'])
+
+            if (typeof mathRef.current !== 'undefined') {
+                mathRef.current.append(mfe)
+            }
+        }
+
+        a()
+    }, [])
+
+    return <>
+        <NoSSREditor
+            wrapperClassName="demo-wrapper"
+            editorClassName="demo-editor"
+            // eslint-disable-next-line react/jsx-key
+            toolbarCustomButtons={[<CustomOption />]}
+        />
+        <div ref={mathRef}></div>
+
+    </>
 }
 
 type CustomOptionProps = {
