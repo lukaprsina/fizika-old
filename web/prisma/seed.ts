5165a0bf4dd9ac0a4b2c56d91e9e0a48c23142d6
Luka Pršina <prsinaluka@gmail.com>
2022-11-05 14:42:35 +0100

    seed works perfectly

--- web/prisma/seed.ts
+++ web/prisma/seed.ts
@@ -32,7 +32,10 @@ async function main() {
         }
 
         const course_dir = path.join(courses_dir, i.toString());
-        let j = 0;
+        if (!fs.existsSync(course_dir)) {
+            break;
+        }
+
         console.log("course", course_dir)
 
         const config_path = path.join(course_dir, "config.json")
@@ -57,92 +60,90 @@ async function main() {
             where: { value: keyword }
         }))
 
-        if (fs.existsSync(course_dir)) {
-            const topic = await prisma.topic.create({
-                data: {
-                    id: i,
-                    title: config_json.heading,
-                    year: config_json.year,
-                    authors: {
-                        connectOrCreate: authors
+        const topic = await prisma.topic.create({
+            data: {
+                id: i,
+                title: config_json.heading,
+                year: config_json.year,
+                authors: {
+                    connectOrCreate: authors
+                },
+                course: { connect: { id: fizika_course.id } },
+                metadata: {
+                    create: {
+                        description: script_json.metadata.description,
+                        goals: script_json.metadata.goals,
+                        license: script_json.metadata.license,
+                        keywords: {
+                            connectOrCreate: keywords
+                        }
                     },
-                    course: { connect: { id: fizika_course.id } },
-                    metadata: {
-                        create: {
-                            description: script_json.metadata.description,
-                            goals: script_json.metadata.goals,
-                            license: script_json.metadata.license,
-                            keywords: {
-                                connectOrCreate: keywords
+                },
+                resource: {
+                    create: {
+
+                    }
+                }
+            }
+        })
+
+        let j = 0;
+        while (true) {
+            const exercise_dir = path.join(course_dir, `pages/page_${j}`)
+            const exercise_file = path.join(exercise_dir, "index.html")
+
+            if (!fs.existsSync(exercise_file))
+                break;
+
+            const popups_dir = path.join(exercise_dir, "popups");
+            let popup_ids = []
+            console.log("\tpage dir", exercise_dir)
+
+            if (fs.existsSync(popups_dir)) {
+                const contents = fs.readdirSync(popups_dir)
+
+                for (const popup_file of contents) {
+                    const popup_path = path.join(popups_dir, popup_file)
+                    console.log("\t\tpopup", popup_file, popup_path)
+
+                    const file = fs.readFileSync(popup_path);
+                    const popup = await prisma.modal.create({
+                        data: {
+                            html: file.toString(),
+                            text: "Modal text",
+                            heading: "Modal heading"
+                        }
+                    });
+
+                    popup_ids.push({ id: popup.id })
+                }
+            }
+
+            const page_path = path.join(exercise_dir, "config.json")
+            const page_file = fs.readFileSync(page_path).toString();
+            const page_json = JSON.parse(page_file);
+
+            if (fs.existsSync(exercise_file)) {
+                await prisma.page.create({
+                    data: {
+                        id: j,
+                        html: fs.readFileSync(exercise_file).toString(),
+                        text: "Page text",
+                        title: page_json.subheading,
+                        topic: { connect: { id: topic.id } },
+                        resource: {
+                            create: {
+                                modals: {
+                                    connect: popup_ids
+                                }
                             }
                         },
-                    },
-                    resource: {
-                        create: {
-
-                        }
+                        metadata: { create: {} }
                     }
-                }
-            })
-
-            while (true) {
-                const exercise_dir = path.join(course_dir, `pages/page_${j}`)
-                const exercise_file = path.join(exercise_dir, "index.html")
-
-                const popups_dir = path.join(exercise_dir, "popups");
-                let popup_ids = []
-                console.log("\tpage dir", exercise_dir)
-
-                if (fs.existsSync(popups_dir)) {
-                    const contents = fs.readdirSync(popups_dir)
-
-                    for (const popup_file of contents) {
-                        const popup_path = path.join(popups_dir, popup_file)
-                        console.log("\t\tpopup", popup_file, popup_path)
-
-                        const file = fs.readFileSync(popup_path);
-                        const popup = await prisma.modal.create({
-                            data: {
-                                html: file.toString(),
-                                text: "Modal text",
-                                heading: "Modal heading"
-                            }
-                        });
-
-                        popup_ids.push({ id: popup.id })
-                    }
-                }
-
-                const page_path = path.join(exercise_dir, "config.json")
-                const page_file = fs.readFileSync(page_path).toString();
-                const page_json = JSON.parse(page_file);
-
-                if (fs.existsSync(exercise_file)) {
-                    await prisma.page.create({
-                        data: {
-                            id: j,
-                            html: fs.readFileSync(exercise_file).toString(),
-                            text: "Page text",
-                            title: page_json.subheading,
-                            topic: { connect: { id: topic.id } },
-                            resource: {
-                                create: {
-                                    modals: {
-                                        connect: popup_ids
-                                    }
-                                }
-                            },
-                            metadata: { create: {} }
-                        }
-                    })
-                } else {
-                    break;
-                }
-
-                j += 1;
+                })
             }
-        } else {
-            break;
+
+            j += 1;
         }
 
         i += 1;
@@ -158,75 +159,3 @@ main()
         await prisma.$disconnect()
         process.exit(1)
     })
-
-/* const fizika = await prisma.course.create({
-    data: {
-        title: "Fizika",
-        metadata: {
-            create: {
-                description: "Fizika course metadata",
-                keywords: {
-                    create: [
-                        { text: "Elektrika" },
-                        { text: "Nihanje" },
-                    ]
-                }
-            }
-        },
-        resource: {
-            create: {
-
-            }
-        },
-        topics: {
-            create: [
-                {
-                    title: "Nihanje",
-                    subtitle: "Nauči se nihanje",
-                    year: "2015",
-                    metadata: {
-                        create: {
-                            description: "Nihanje description",
-                        }
-                    },
-                    resource: {
-                        create: {
-
-                        }
-                    },
-                    authors: {
-                        create: {
-                            name: "Luka P"
-                        }
-                    },
-                    pages: {
-                        create: [
-                            {
-                                html: "<div>a</div>",
-                                text: "a",
-                                metadata: {
-                                    create: {
-                                        description: "Page description"
-                                    }
-                                },
-                                resource: {
-                                    create: {
-                                        modals: {
-                                            create: [
-                                                {
-                                                    html: "<div>modal</div>",
-                                                    text: "modal",
-                                                    heading: "Modal on page"
-                                                }
-                                            ]
-                                        }
-                                    }
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-    }
-}) */
