34ba0f2634535c214c06832640cf18e5d07b34a6
Luka <prsinaluka@gmail.com>
2022-11-15 08:07:25 +0000

    solid works and runs

--- solid/src/routes/index.tsx
+++ solid/src/routes/index.tsx
@@ -1,28 +1,5 @@
-import { refetchRouteData, useRouteData } from "solid-start";
-import { createServerAction$ } from "solid-start/server";
-import { logout } from "~/db/session";
-import { useUser } from "../db/useUser";
-
-export function routeData() {
-  return useUser();
-}
-
 export default function Home() {
-  const user = useRouteData<typeof routeData>();
-  const [, { Form }] = createServerAction$((f: FormData, { request }) =>
-    logout(request)
-  );
-
-  return (
-    <main class="w-full p-4 space-y-2">
-      <h1 class="font-bold text-3xl">Hello {user()?.username}</h1>
-      <h3 class="font-bold text-xl">Message board</h3>
-      <button onClick={() => refetchRouteData()}>Refresh</button>
-      <Form>
-        <button name="logout" type="submit">
-          Logout
-        </button>
-      </Form>
-    </main>
-  );
+    return (
+        <h1 class="">Welcome, welcome to city 17.</h1>
+    )
 }
