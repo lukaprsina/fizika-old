85e125196967cc438b50093b76d51dec403fa458
Luka Pršina <prsinaluka@gmail.com>
2023-01-01 19:29:59 +0100

    mdx provider isn't working

--- web/src/routes/mdx.tsx
+++ web/src/routes/mdx.tsx
@@ -1,26 +1,74 @@
-import type { Component } from "solid-js";
+import type { Component, JSX, ParentComponent } from "solid-js";
+import { Show, createSignal } from "solid-js";
 import { onMount } from "solid-js"
 import { compile, run } from '@mdx-js/mdx'
-import * as runtime from 'solid-js/jsx-runtime'
+import * as jsx_runtime from 'solid-jsx'
+import { MDXProvider } from 'solid-jsx'
 
-const MDX = () => {
+const MDX: Component = () => {
+    const [Content, setContent] = createSignal<JSX.Element>();
+
     onMount(async () => {
+        const text = `# Heading (rank 1)
+## Heading 2
+### 3
+#### 4
+##### 5
+###### 6
+
+> Block quote
+
+* Unordered
+* List
+
+1. Ordered
+2. List
+
+A paragraph, introducing a thematic break:
+
+---
+
+\`\`\`js
+some.code()
+\`\`\`
+
+<Test>OMG</Test>
+
+a [link](https://example.com), an ![image](./image.png), some *emphasis*,
+something **strong**, and finally a little \`code()\`.
+        `;
+
         const code = String(await compile(
-            '# hi',
+            text,
             {
                 outputFormat: 'function-body',
-                jsxImportSource: 'solid-js'
+                jsxImportSource: 'solid-jsx',
+                // providerImportSource: 'solid-jsx',
+                // format: 'mdx',
+                // useDynamicImport: true,
+                // development: true,
             }
         ))
 
-        console.log(code)
-        const { default: Content } = await run(code, runtime);
-        console.log(Content.toString())
-
-        const a = <Content />
+        const JSXContent = (await run(code, jsx_runtime)).default;
+        setContent(JSXContent)
     })
 
-    return <>lol</>
+    return (
+        <MDXProvider
+            components={{
+                ["Test"]: H1Test
+            }}
+        >
+            <Show when={Content}>
+                {Content()}
+            </Show>
+        </MDXProvider>
+    )
+}
+
+const H1Test: ParentComponent = (props) => {
+    return <h4>{props.children}</h4>
 }
 
 export default MDX
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,14 +2,11 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import mdx from '@mdx-js/rollup';
-import remarkGfm from 'remark-gfm';
 
 export default defineConfig(() => {
   dotenv.config();
   return {
     plugins: [
-      mdx({ jsxImportSource: 'solid-jsx', remarkPlugins: [remarkGfm] }),
       solid({ /* ssr: true */ }),
       viteStaticCopy({
         targets: [
