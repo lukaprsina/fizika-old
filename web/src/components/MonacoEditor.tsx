ce25a0a1f93ec63e7d679cffaa3b7d4e2be6bee9
Luka Pršina <luk.prsina@gmail.com>
2023-01-07 22:12:45 +0100

    file upload works, no folders

--- web/src/components/MonacoEditor.tsx
+++ web/src/components/MonacoEditor.tsx
@@ -1,24 +1,57 @@
 import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
+import { Body } from "solid-start";
 import { onMount } from "solid-js";
 import loader from '@monaco-editor/loader';
+import { createDropzone } from "@solid-primitives/upload";
+import type monaco from 'monaco-editor'
+
+/* const selection = editor().getPosition();
+
+    editor().executeEdits("file upload", [{
+        range: selection?.collapseToStart(),
+        text: "XXX",
+        forceMoveMarkers: true
+    }]) */
 
 const MonacoEditor: Component = () => {
+    const [editor, setEditor] = createSignal<monaco.editor.IStandaloneCodeEditor>()
+
+    const { setRef: dropzoneRef } = createDropzone({
+        onDrop: async files => {
+            const formData = new FormData();
+            files.forEach(file => formData.append("files", file.file));
+
+            await fetch("api/upload", {
+                method: "POST",
+                headers: new Headers({
+                    'content-type': 'multipart/form-data'
+                }),
+                body: formData,
+            })
+        }
+    })
+
     onMount(() => {
         loader.init().then(monaco => {
             const component = document.querySelector("#editor");
             if (!component)
                 return;
 
-            monaco.editor.create(component as HTMLElement, {
+            const new_editor = monaco.editor.create(component as HTMLElement, {
                 value: '# editor',
                 language: 'markdown',
+                dragAndDrop: true,
             });
+
+            setEditor(new_editor);
         });
     })
 
     return <div
         id="editor"
-        class="w-screen h-screen bg-gray-400"
+        ref={dropzoneRef}
+        class="w-screen h-screen"
     />
 }
 
--- web/src/entry-server.tsx
+++ web/src/entry-server.tsx
@@ -1,9 +1,16 @@
+// import multer from "multer";
 import {
   StartServer,
   createHandler,
   renderAsync,
 } from "solid-start/entry-server";
 
+// const upload = multer({ dest: "MULTER_UPLOAD" })
+// const middleware = upload.array("avatar");    
+
 export default createHandler(
+  //   (input) => {
+  // return middleware()
+  //   },
   renderAsync((event) => <StartServer event={event} />)
 );
