6cb3db9ffb546986e27bbb670be4a158a018243f
Luka <luk.prsina@gmail.com>
2023-01-23 08:27:31 +0000

    long time no see

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::rc::Rc;
 
 use color_eyre::eyre::Result;
-use itertools::Itertools;
-use math_eval::ast::{app::App, context::Context};
-use tracing::Level;
+use math_eval::{
+    actions::is_same::{IsSame, IsSameNames},
+    ast::{app::App, context::Context, Element},
+};
+use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 // TODO: vec remove unwrap
@@ -35,8 +37,8 @@ fn main() -> Result<()> {
 
     // let a = "2log(a+c)+b";
     // let b = "b+2log(c+b)";
-    let a = "1 + 2 + 3";
-    let b = "5 + 6";
+    let a = "sin(x) + 4";
+    let b = "(sin(x)) + 2";
 
     let uuid1 = App::try_add_equation(Rc::clone(&app), ctx_uuid, a)?;
     let uuid2 = App::try_add_equation(Rc::clone(&app), ctx_uuid, b)?;
@@ -46,24 +48,21 @@ fn main() -> Result<()> {
         // let ctx_uuid = borrowed_app.formulas;
         let ctx = borrowed_app.get_context_mut(ctx_uuid).unwrap();
 
-        // ctx.solve();
-
         let eq1 = ctx.remove_equation(uuid1).unwrap();
         let eq2 = ctx.remove_equation(uuid2).unwrap();
 
-        // println!("{:#?}", eq1);
-        // info!(%eq1);
-        // info!(%eq2);
+        info!(%eq1);
+        info!(%eq2);
 
         let elem1 = eq1.eq_sides.first().unwrap();
         let elem2 = eq2.eq_sides.first().unwrap();
 
         elem1.bind(&elem2);
 
-        /* let mut names = IsSameNames::new();
+        let mut names = IsSameNames::new();
         let is_same = Element::is_same(&elem1, &elem2, &mut names);
         println!("{:#?}\n", names);
-        info!("check: {}, is_same: {}", names.check(), is_same); */
+        info!("check: {}, is_same: {}", names.check(), is_same);
     }
 
     Ok(())
